b231b4c0d207e8defd688a62adad9b6b86c15961
Daniel Gysi <danielgysi@protonmail.com>
2024-12-04 18:03:16 -0500

    day 4 grid graph once again

--- 04/1/aoc04lvl1/src/lib.rs
+++ 04/1/aoc04lvl1/src/lib.rs
@@ -48,16 +48,75 @@ pub fn run(cfg: Config) -> anyhow::Result<u32> {
 
     let data = parse(input_string)?;
     let result = process(data);
+    println!("{}", result);
 
     Ok(result)
 }
 
-fn parse(input: String) -> anyhow::Result<String> {
-    // remember to change the return type
-    todo!()
+fn parse(input: String) -> anyhow::Result<Vec<Vec<char>>> {
+    Ok(input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect())
 }
 
-fn process(data: String) -> u32 {
-    // remember to change the param type
-    todo!()
+fn process(data: Vec<Vec<char>>) -> u32 {
+    const GOAL: [char; 4] = ['X', 'M', 'A', 'S'];
+    let mut match_count = 0;
+    let max_x = data.len() - 1;
+    let max_y = data[0].len() - 1;
+    for (x, row) in data.iter().enumerate() {
+        for (y, ch) in row.iter().enumerate() {
+            if *ch == GOAL[0] {
+                for line in get_line_coords((x, y), GOAL.len(), (max_x, max_y)) {
+                    if line
+                        .into_iter()
+                        .map(|(lx, ly)| data[lx][ly])
+                        .collect::<Vec<char>>()
+                        == GOAL[1..GOAL.len()]
+                    {
+                        match_count += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    match_count
+}
+
+fn get_line_coords(
+    coord: (usize, usize),
+    len: usize,
+    max: (usize, usize),
+) -> Vec<Vec<(usize, usize)>> {
+    let (x, y) = coord;
+    let (x, y) = (x as i32, y as i32);
+    let (max_x, max_y) = max;
+    let (max_x, max_y) = (max_x as i32, max_y as i32);
+    let mut lines = vec![vec!(); 8];
+    for i in 1..len {
+        let i = i as i32;
+        lines[0].push((x + i, y));
+        lines[1].push((x, y + i));
+        lines[2].push((x + i, y + i));
+        lines[3].push((x - i, y));
+        lines[4].push((x, y - i));
+        lines[5].push((x - i, y - i));
+        lines[6].push((x + i, y - i));
+        lines[7].push((x - i, y + i));
+    }
+    lines
+        .into_iter()
+        .filter(|line| {
+            line.iter()
+                .map(|(x, y)| 0 <= *x && *x <= max_x && 0 <= *y && *y <= max_y)
+                .all(|v| v)
+        })
+        .map(|line| {
+            line.iter()
+                .map(|(x, y)| (*x as usize, *y as usize))
+                .collect()
+        })
+        .collect()
 }
--- 04/2/aoc04lvl2/src/lib.rs
+++ 04/2/aoc04lvl2/src/lib.rs
@@ -48,16 +48,90 @@ pub fn run(cfg: Config) -> anyhow::Result<u32> {
 
     let data = parse(input_string)?;
     let result = process(data);
+    println!("{}", result);
 
     Ok(result)
 }
 
-fn parse(input: String) -> anyhow::Result<String> {
-    // remember to change the return type
-    todo!()
+fn parse(input: String) -> anyhow::Result<Grid> {
+    Ok(Grid {
+        data: input
+            .lines()
+            .map(|line| line.chars().collect::<Vec<_>>())
+            .collect(),
+    })
 }
 
-fn process(data: String) -> u32 {
-    // remember to change the param type
-    todo!()
+fn process(data: Grid) -> u32 {
+    let mut goal = vec!['M', 'A', 'S'];
+    goal.sort();
+    let mut match_count = 0;
+
+    for x in 0..data.max_x() {
+        for y in 0..data.max_y() {
+            if x > 0 && y > 0 && data.get(Coord { x, y }) == 'A' {
+                let top_left = Coord { x: x - 1, y: y - 1 };
+                let line1 = data.get_line(&top_left, 1, 1, 3);
+                let bottom_left = Coord { x: x - 1, y: y + 1 };
+                let line2 = data.get_line(&bottom_left, 1, -1, 3);
+
+                if let (Some(mut chrs1), Some(mut chrs2)) = (line1, line2) {
+                    chrs1.sort();
+                    chrs2.sort();
+                    if chrs1 == goal && chrs2 == goal {
+                        match_count += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    match_count
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Coord {
+    x: usize,
+    y: usize,
+}
+struct Grid {
+    data: Vec<Vec<char>>,
+}
+impl Grid {
+    fn get(&self, coord: Coord) -> char {
+        self.data[coord.y][coord.x]
+    }
+    fn is_valid_coord(&self, coord: &Coord) -> bool {
+        coord.x < self.max_x() && coord.y < self.max_y()
+    }
+    fn get_line(&self, start: &Coord, x_inc: i32, y_inc: i32, length: usize) -> Option<Vec<char>> {
+        if !self.is_valid_coord(start) {
+            return None;
+        }
+        let mut out = vec![self.get(*start)];
+        let mut cur = *start;
+        for _ in 1..length {
+            let new_x = cur.x as i32 + x_inc;
+            let new_y = cur.y as i32 + y_inc;
+            if new_x < 0 || new_y < 0 {
+                return None;
+            }
+            cur = Coord {
+                x: new_x as usize,
+                y: new_y as usize,
+            };
+            if !self.is_valid_coord(&cur) {
+                return None;
+            }
+            out.push(self.get(cur));
+        }
+
+        Some(out)
+    }
+    fn max_x(&self) -> usize {
+        self.data[0].len()
+    }
+    fn max_y(&self) -> usize {
+        self.data.len()
+    }
 }
